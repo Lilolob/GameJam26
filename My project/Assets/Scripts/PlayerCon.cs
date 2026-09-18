f015aad24c56e0aba364fe7f27487c846b1889dd
Lilly Gregory <tcgreg91@gmail.com>
2026-09-18 11:43:36 +0100

    Added gameloops

---
+++ My project/Assets/Scripts/PlayerCon.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerCon : MonoBehaviour
+{
+
+    public GameObject Solider1;
+
+    public GameObject SpawnPoint;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            
+        }
+    }
+}
