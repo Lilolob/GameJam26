11e23fc17dd44922d4d61d8a7c8936a61fd4ec44
Lilly Gregory <tcgreg91@gmail.com>
2026-09-18 14:26:00 +0100

    Revert "Merge branch 'main' of https://github.com/Lilolob/GameJam26"
    This reverts commit 4ab597670f2a6baa93cf4e8b2320905e00ff0045, reversing
    changes made to 6230bb28fea6815110119d7d0e928d5a356f8637.

--- My project/Assets/Scripts/Soldier1.cs
+++ My project/Assets/Scripts/Soldier1.cs
@@ -16,12 +16,11 @@ public class Soldier1 : MonoBehaviour
     public soldierState currentState;
 
     public int health = 10;
-    public int damage = 5;
+    public int damage = 1;
     public int reloadTime = 1;
     public int reloadSpeed = 1;
     public int moveSpeed = 5;
     public int range = 1;
-
 
     public void takeDamage(int damage)
     {
@@ -60,20 +59,8 @@ public class Soldier1 : MonoBehaviour
     void UpdateWalking()
     {
 
-        // Move player at constant speed to the right
+        // Move player at cnstant speed to the right
         transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-    }
 
-    void UpdateAttacking()
-    {
-        
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Enemy"))
-        {
-            currentState = soldierState.Attacking;
-        }  
     }
 }
