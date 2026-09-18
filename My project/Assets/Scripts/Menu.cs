0e8c2147efac97a2f6e6bb7ca8f67f8d4c76062b
Lilly Gregory <tcgreg91@gmail.com>
2026-09-18 14:08:53 +0100

    added stuff

--- My project/Assets/Scripts/Menu.cs
+++ My project/Assets/Scripts/Menu.cs
@@ -5,14 +5,12 @@ public class Menu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    void 
+    }
 }
